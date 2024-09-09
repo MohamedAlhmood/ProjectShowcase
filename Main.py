465dd646a1e42be7e24addb04370a34dd064abf9
Mohamed Alhmood <Moealhmood21@gmail.com>
2024-09-09 18:34:42 -0400

    Rendered App titles in two rows.

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import streamlit as st
-
+import pandas
 st.set_page_config(layout='wide')
 col1,col2 = st.columns(2)
-
+csvData = pandas.read_csv('data.csv',sep=';')
 with col1:
     st.image('images/image.jpg')
 with col2:
@@ -11,3 +11,11 @@ with col2:
     st.info(content)
 content2 = """***Below are some of my projects. Feel free to contact me!***"""
 st.write(content2)
+
+col3 , col4 = st.columns(2)
+with col3:
+    for index,row in csvData[:10].iterrows():
+        st.header(row['title'])
+with col4:
+    for index,row in csvData[10:].iterrows():
+        st.header(row['title'])
